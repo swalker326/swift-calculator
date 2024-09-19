59b3956742c6474bbfeafdf3878e6f3960bed631
Shane <shane@swalker.dev>
2024-09-18 20:49:12 -0500

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Shane Walker on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ calculatorTests/calculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorTests.swift
+//  calculatorTests
+//
+//  Created by Shane Walker on 9/18/24.
+//
+
+import Testing
+@testable import calculator
+
+struct calculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
